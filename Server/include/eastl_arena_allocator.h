b9d7bcf20eb0223849162b271c49bc41d56f2956
Fatih Bakal <fatihbakal7@protonmail.com>
2026-04-27 03:37:26 +0300

    hopefully fix fr?

--- Server/include/eastl_arena_allocator.h
+++ Server/include/eastl_arena_allocator.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <EASTL/internal/config.h>
-
 #define FB_MEMORYARENA_ALLOC_ADDRESS CYPRESS_GW_SELECT(0x140389540, 0x140155630, 0x140416050)
 #define FB_MEMORYARENA_FREE_ADDRESS CYPRESS_GW_SELECT(0x140389FC0, 0x140155740, 0x140417740)
 #define FB_MEMORYARENA_FINDARENAFOROBJECT_ADDRESS CYPRESS_GW_SELECT(0x140389E70, 0x1401CA550, 0x14042FE10)
-
 namespace fb
 {
-
 	class eastl_arena_allocator
 	{
 	public:
@@ -16,25 +13,18 @@ namespace fb
 		eastl_arena_allocator(void* memoryArena);
 		eastl_arena_allocator(const eastl_arena_allocator& x);
 		eastl_arena_allocator(const eastl_arena_allocator& x, const char* pName);
-
 		eastl_arena_allocator& operator=(const eastl_arena_allocator& x);
-		bool operator==(const eastl_arena_allocator& x);
-
+		bool operator==(const eastl_arena_allocator& x) const;
 		void* allocate(size_t n, int flags = 0);
 		void* allocate(size_t n, size_t alignment, size_t offset, int flags = 0);
 		void  deallocate(void* p, size_t n);
-
 		const char* get_name() const;
 		void        set_name(const char* pName);
-
 		const void* get_arena() const;
 		void		set_arena(void* pArena);
-
 	protected:
 		void* mpArena;
 	};
-
 	eastl_arena_allocator* GetDefaultAllocator();
 	eastl_arena_allocator* SetDefaultAllocator(eastl_arena_allocator* pAllocator);
-
 } // namespace fb
